23808290b3c844282f2330ad28648989eab8a297
C5m7b4 <mbedingfield@dcrpos.com>
2021-06-01 12:33:13 -0500

    starts margin story

--- src/stories/Margins.stories.js
+++ src/stories/Margins.stories.js
@@ -17,7 +17,8 @@ const Template = args => {
       {Object.keys(args).map((arg, i) => (
         <div
           key={`mg-demo=${i}`}
-          className={`styleguide-demo-each secondary-color-bg ${args[arg]} spacing`}
+          className={` ${args[arg]} primary`}
+          style={{ padding: '10px', color: '#fff' }}
           {...args}
         >
           This is some sample Text using the{' '}
@@ -30,32 +31,5 @@ const Template = args => {
 
 export const Margins = Template.bind({});
 Margins.args = {
-  mg1: 'mg-1',
-  mg2: 'mg-1-h',
-  mg3: 'mg-1-v',
-  mg4: 'mg-1-l',
-  mg5: 'mg-1-r',
-  mg6: 'mg-1-t',
-  mg7: 'mg-1-b',
-  mg8: 'mg-2',
-  mg9: 'mg-2-h',
-  mg10: 'mg-2-v',
-  mg11: 'mg-2-l',
-  mg12: 'mg-2-r',
-  mg13: 'mg-2-t',
-  mg14: 'mg-2-b',
-  mg15: 'mg-3',
-  mg16: 'mg-3-h',
-  mg17: 'mg-3-v',
-  mg18: 'mg-3-l',
-  mg19: 'mg-3-r',
-  mg20: 'mg-3-t',
-  mg21: 'mg-3-b',
-  mg22: 'mg-half',
-  mg23: 'mg-half-h',
-  mg24: 'mg-half-v',
-  mg25: 'mg-half-l',
-  mg26: 'mg-half-r',
-  mg27: 'mg-half-t',
-  mg28: 'mg-half-b',
+  'm-0': 'm-0',
 };
